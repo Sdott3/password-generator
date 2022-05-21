39ecef8d6555c9110f658f8c9c23e2def97e3655
sdott3 <sdotthomas347@gmail.com>
2022-05-21 11:08:43 -0400

    arrays created for password criteria

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,16 @@
 // Assignment code here
-
+// arrays created containg password criteria
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k','l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numeric = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var specialCharacter = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+var passwordLength;
+var allChar = [];
+var newPassword ="";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
